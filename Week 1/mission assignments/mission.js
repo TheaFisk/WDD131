992e1630cbdf8547c05467d8fd1276360e68d2de
TheaFisk <theafisk@gmail.com>
2025-05-03 23:18:57 -0600

    party mode

--- Week 1/mission assignments/mission.js
+++ Week 1/mission assignments/mission.js
@@ -9,12 +9,16 @@ function changeTheme() {
     const logo = document.getElementById('logo');
 
     if (selectedTheme === 'dark') {
+        body.classList.remove('party')
         body.classList.add('dark');
         logo.src = 'byui-logo_white.png'; 
     } else if (selectedTheme == 'party') {
+        body.classList.remove('dark')
         body.classList.add('party')
-        logo.src = 'party-time.gif';
+        logo.src = 'byui-logo_blue.webp'; 
+        //logo.src = 'party-time.gif';
     } else {
+        body.classList.remove('party')
         body.classList.remove('dark');
         logo.src = 'byui-logo_blue.webp';
     }
