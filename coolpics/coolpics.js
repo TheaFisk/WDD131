2a824d888eadb8ff6e3cd74efc54c78de20240a7
TheaFisk <theafisk@gmail.com>
2025-05-20 10:45:50 -0600

    Changed my mind

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -8,3 +8,38 @@ document.addEventListener("DOMContentLoaded", () => {
     nav.classList.toggle("hide");
   });
 });
+
+
+const gallery = document.querySelector(".gallery");
+gallery.addEventListener("click", viewHandler);
+
+function viewHandler(event) {
+  if (event.target.tagName === 'IMG') {
+    const imageSrc = event.target.src;
+    const imageParts = imageSrc.split("-");
+    const fullImageSrc = imageParts[0] + "-full.jpeg";
+
+    const dialog = document.createElement("dialog");
+    dialog.classList.add("image-modal");
+
+    dialog.innerHTML = `
+      <img src="${fullImageSrc}" alt="${event.target.alt}">
+      <button class="close-viewer" aria-label="Close modal">X</button>
+    `;
+
+    document.body.appendChild(dialog);
+    dialog.showModal();
+
+    dialog.querySelector(".close-viewer").addEventListener("click", () => {
+      dialog.close();
+      dialog.remove();
+    });
+
+    dialog.addEventListener("click", (e) => {
+      if (e.target === dialog) {
+        dialog.close();
+        dialog.remove();
+      }
+    });
+  }
+}
